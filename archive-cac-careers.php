e585d0a88088a897b34c0a0d84834d2d820455fa
cat <macy.cordner@gmail.com>
2023-11-12 15:23:19 -0800

    added content to careers archive, need to fix content output as well as link for apply now. Also need to add buttons for filtering

--- archive-cac-careers.php
+++ archive-cac-careers.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying archive pages
+ * The template for displaying Careers Archive
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -10,42 +10,75 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php if (have_posts()): ?>
 
 			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				<h1 class="page-title">
+					<?php post_type_archive_title(); ?>
+				</h1>
+			</header>
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+		// 	while (have_posts()):
+		// 		the_post();
+	
+		// 		// Include the content template
+		// 		get_template_part('template-parts/content', 'archive');
+	
+		// 	endwhile;
+	
+
+		// else:
+	
+		// 	// If no content, include the "No posts found" template.
+		// 	get_template_part('template-parts/content', 'none');
+	
+	endif;
+	?>
+
+		<?php
+
+		$args = array(
+			'post_type' => 'cac-careers',
+			'posts_per_page' => -1,
+		);
+
+		$careers_query = new WP_Query($args);
+
+		if ($careers_query->have_posts()):
+			while ($careers_query->have_posts()):
+				$careers_query->the_post();
+
+				$location_name = get_field('location_name');
+				$job_descriptions = get_field('job_descriptions');
+				//dont need
+				$job_requirements = get_field('job_requirements');
+				$job_cta = get_field('job_cta');
+
+				echo '<div class="job-description">';
+				echo '<h2>' . esc_html(get_the_title()) . '</h2>';
+				echo '<p>' . esc_html($location_name) . '</p>';
+				echo '<p><strong>Description:</strong> ' . esc_html($job_descriptions) . '</p>';
+				echo '<p>Requirements: ' . esc_html($job_requirements) . '</p>';
+				echo '<a href="' . esc_url($job_cta) . '">Apply Now</a>';
+				echo '</div>';
 
 			endwhile;
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
+			wp_reset_postdata();
+		else:
+			echo 'No Careers found.';
 		endif;
 		?>
 
 	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_sidebar();
 get_footer();
+?>
