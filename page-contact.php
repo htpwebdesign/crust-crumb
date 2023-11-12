3b6e733a6a5965f28659dac0a5b35f8cb07fc29a
Behnam Arabi <behnam.mailbox@gmail.com>
2023-11-12 12:36:15 -0800

    The location image, previously an ACF, has been replaced with a field in a CPT

--- page-contact.php
+++ page-contact.php
@@ -16,64 +16,67 @@ get_header();
         the_post();
 
         // Display Page Title
-        if( get_field('page_title') ): 
+        if ( get_field('page_title') ): 
             echo '<h1>' . get_field('page_title') . '</h1>';
         endif;
 
-        // Display Store Locations
-        if( have_rows('store_locations') ): 
+        // Query location-CPT posts
+        $location_posts = new WP_Query(array(
+            'post_type' => 'cac-store-locations', 
+            'posts_per_page' => -1 
+        ));
+
+        if ( $location_posts->have_posts() ) :
             echo '<div class="store-locations">';
-            while ( have_rows('store_locations') ) : the_row();
-                $store_image_id = get_sub_field('store_image');
-                $location_name = get_sub_field('location_name');
-                $location_hours = get_sub_field('location_hours');
-                $location_phone_number = get_sub_field('location_phone_number');
-                $location_address = get_sub_field('location_address');
+            while ( $location_posts->have_posts() ) : $location_posts->the_post();
+
+                // Fetch fields for each location
+                
+                $location_name = get_field('location_name');
+                $location_hours = get_field('location_hours');
+                $location_phone = get_field('location_phone');
+                $location_address = get_field('location_address');
 
                 echo '<div class="store">';
-                if( $store_image_id ) {
-                    $store_image_url = wp_get_attachment_image_url( $store_image_id, 'full' );
-                    $store_image_alt = get_post_meta( $store_image_id, '_wp_attachment_image_alt', true );
-                    echo '<img src="' . esc_url($store_image_url) . '" alt="' . esc_attr($store_image_alt) . '" />';
+                // Display location image
+                $location_image_id = get_post_meta( get_the_ID(), 'location_image', true ); 
+                if ( $location_image_id ) {
+                    $location_image_url = wp_get_attachment_image_url( $location_image_id, 'full' );
+                    echo '<img src="' . esc_url($location_image_url) . '" alt="' . esc_attr(get_the_title()) . '" />';
                 }
-                if( $location_name ) {
-                    echo '<h2>' . esc_html($location_name) . '</h2>';
-                }
-                if( $location_hours ) {
-                    echo '<p>Hours: ' . esc_html($location_hours) . '</p>';
-                }
-                if( $location_phone_number ) {
-                    echo '<p>Phone: ' . esc_html($location_phone_number) . '</p>';
-                }
-                if( $location_address ) {
-                    echo '<p>Address: ' . esc_html($location_address) . '</p>';
-                }
+                // Display other location details
+                echo '<h2>' . esc_html($location_name) . '</h2>';
+                echo '<p>Hours: ' . esc_html($location_hours) . '</p>';
+                echo '<p>Phone: ' . esc_html($location_phone) . '</p>';
+                echo '<p>Address: ' . esc_html($location_address) . '</p>';
                 echo '</div>';
+
             endwhile;
             echo '</div>';
         endif;
+        wp_reset_postdata();
 
         // Display Store Locations Map
-        if( get_field('store_locations_map') ): 
+        if ( get_field('store_locations_map') ): 
             echo '<div class="store-locations-map">' . get_field('store_locations_map') . '</div>';
         endif;
 
         // Display Social Media Message
-        if( get_field('social_media_message') ): 
+        if ( get_field('social_media_message') ): 
             echo '<div class="social-media-message">' . get_field('social_media_message') . '</div>';
         endif;
 
         // Display Social Media Links
-        if( have_rows('social_media_links') ): 
+        if ( have_rows('social_media_links') ): 
             echo '<div class="social-media-links">';
             while ( have_rows('social_media_links') ) : the_row();
                 $social_media_link_url = get_sub_field('social_media_link');
                 $social_media_image_id = get_sub_field('social_media_image');
 
                 echo '<div class="social-media-item">';
-                if( $social_media_link_url ) {
+                if ( $social_media_link_url ) {
                     echo '<a href="' . esc_url($social_media_link_url) . '">';
-                    if( $social_media_image_id ) {
+                    if ( $social_media_image_id ) {
                         $social_media_image_url = wp_get_attachment_image_url( $social_media_image_id, 'full' );
                         $social_media_image_alt = get_post_meta( $social_media_image_id, '_wp_attachment_image_alt', true );
                         echo '<img src="' . esc_url($social_media_image_url) . '" alt="' . esc_attr($social_media_image_alt) . '" />';
@@ -82,9 +85,11 @@ get_header();
                 }
                 echo '</div>';
             endwhile;
+            
+            gravity_form( 1, false, false, false, '', false );
             echo '</div>';
         endif;
-
+        
         // The default page content
         get_template_part( 'template-parts/content', 'page' );
 
@@ -95,7 +100,6 @@ get_header();
 
     endwhile; // End of the loop.
     ?>
-<?php gravity_form( 1, false, false, false, '', false ); ?>
 
 </main>
 
