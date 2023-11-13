2af9c1602f18c6011c7ca265a921f682093f9068
cat <macy.cordner@gmail.com>
2023-11-12 16:09:11 -0800

    added filtering

--- archive-cac-careers.php
+++ archive-cac-careers.php
@@ -21,27 +21,9 @@ get_header();
 				</h1>
 			</header>
 
-			<?php
-
-		// 	while (have_posts()):
-		// 		the_post();
-	
-		// 		// Include the content template
-		// 		get_template_part('template-parts/content', 'archive');
-	
-		// 	endwhile;
-	
-
-		// else:
-	
-		// 	// If no content, include the "No posts found" template.
-		// 	get_template_part('template-parts/content', 'none');
-	
-	endif;
-	?>
+		<?php endif; ?>
 
 		<?php
-
 		$args = array(
 			'post_type' => 'cac-careers',
 			'posts_per_page' => -1,
@@ -49,28 +31,64 @@ get_header();
 
 		$careers_query = new WP_Query($args);
 
-		if ($careers_query->have_posts()):
-			while ($careers_query->have_posts()):
-				$careers_query->the_post();
+		if ($careers_query->have_posts()): ?>
 
-				$location_name = get_field('location_name');
-				$job_descriptions = get_field('job_descriptions');
-				//dont need
-				$job_requirements = get_field('job_requirements');
-				$job_cta = get_field('job_cta');
+			<form>
+				<?php
+				$store_locations = get_posts(
+					array(
+						'post_type' => 'cac-careers',
+						'posts_per_page' => -1,
+						'meta_key' => 'location_name',
+						'fields' => 'ids',
+						'orderby' => 'meta_value',
+						'order' => 'ASC',
+					)
+				);
+//array_unique removes all duplicates and array_map takes the post id and returns the location name 
+				$store_locations = array_unique(array_map(function ($post_id) {
+					return get_field('location_name', $post_id);
+				}, $store_locations));
 
-				echo '<div class="job-description">';
-				echo '<h2>' . esc_html(get_the_title()) . '</h2>';
-				echo '<p>' . esc_html($location_name) . '</p>';
-				echo '<p><strong>Description:</strong> ' . esc_html($job_descriptions) . '</p>';
-				echo '<p>Requirements: ' . esc_html($job_requirements) . '</p>';
-				echo '<a href="' . esc_url($job_cta) . '">Apply Now</a>';
-				echo '</div>';
+				// Output radio buttons
+				foreach ($store_locations as $location): ?>
+					<label>
+						<input type="radio" name="location" value="<?php echo esc_attr($location); ?>">
+						<?php echo esc_html($location); ?>
+					</label>
+				<?php endforeach; ?>
+				<label>
+					<input type="radio" name="location" value="All" checked> All
+				</label>
 
-			endwhile;
+			</form>
 
-			wp_reset_postdata();
-		else:
+			<div id="filtered-jobs">
+				<?php
+				while ($careers_query->have_posts()):
+					$careers_query->the_post();
+
+					$location_name = get_field('location_name');
+					$job_descriptions = get_field('job_descriptions');
+					//dont need
+					$job_requirements = get_field('job_requirements');
+					$job_cta = get_field('job_cta');
+
+					echo '<div class="job-information">';
+					echo '<h2>' . esc_html(get_the_title()) . '</h2>';
+					echo '<p class="location">' . esc_html($location_name) . '</p>';
+					echo '<p><strong>Description:</strong> ' . esc_html($job_descriptions) . '</p>';
+					echo '<p>Requirements: ' . esc_html($job_requirements) . '</p>';
+					echo '<a href="' . esc_url($job_cta) . '">Apply Now</a>';
+					echo '</div>';
+
+				endwhile;
+
+				wp_reset_postdata();
+				?>
+			</div>
+
+		<?php else:
 			echo 'No Careers found.';
 		endif;
 		?>
---
+++ js/filter-jobs.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Select all radio buttons with the name "location"
+    const radioButtons = document.querySelectorAll('input[name="location"]');
+
+    // Add an event listener to each radio button
+    radioButtons.forEach(function (radio) {
+        radio.addEventListener('change', function () {
+
+            // Get the value of the selected radio button
+            const selectedLocation = document.querySelector('input[name="location"]:checked').value;
+
+            // Select all elements with the class "job-information"
+            const jobDescriptions = document.querySelectorAll('.job-information');
+
+            // Iterate over each job description element
+            jobDescriptions.forEach(function (job) {
+
+                // get location ACF and trim any whitespace if needed
+                const location = job.querySelector('.location').textContent.trim();
+
+                // Check if the selected location is "All" or if the job location includes the selected location
+                if (selectedLocation === 'All' || location.toLowerCase().includes(selectedLocation.toLowerCase())) {
+
+                    // display jobs with selectedLocation
+                    job.style.display = 'block';
+                } else {
+
+                    // hide jobs that dont have selectedLocation
+                    job.style.display = 'none';
+                }
+            });
+        });
+    });
+});
