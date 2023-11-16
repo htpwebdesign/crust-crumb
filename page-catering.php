560cdac5d03d31a55c4363a1f9f5772b1de66379
cat <macy.cordner@gmail.com>
2023-11-16 12:53:48 -0800

    Footer: removed powered by wordpress, added in placeholder for our linkedins. Careers: added if function_exists(get_field), changed divs to sections, and added article for locations. Changed div to p tag for the social media message. Front Page: Removed blank article code. Functions.php: removed widget, moved isotopes into the functions section

--- page-catering.php
+++ page-catering.php
@@ -1,7 +1,5 @@
 <?php
-/*
- * Template Name: Catering Page
- */
+
 
 get_header();
 
--- page-contact.php
+++ page-contact.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Template Name: Contact Page
- * The template for displaying the contact page
- *
- * @package Crust_&_Crumb_Bakery
- */
+
 
 get_header();
 ?>
@@ -12,94 +7,98 @@ get_header();
 <main id="primary" class="site-main">
 
     <?php
-    while ( have_posts() ) :
+    while (have_posts()):
         the_post();
+        if (function_exists("get_field")) {
+            // Display Page Title
+            if (get_field('page_title')):
+                echo '<h1>' . get_field('page_title') . '</h1>';
+            endif;
 
-        // Display Page Title
-        if ( get_field('page_title') ): 
-            echo '<h1>' . get_field('page_title') . '</h1>';
-        endif;
+            // Query location-CPT posts
+            $location_posts = new WP_Query(
+                array(
+                    'post_type' => 'cac-store-locations',
+                    'posts_per_page' => -1
+                )
+            );
 
-        // Query location-CPT posts
-        $location_posts = new WP_Query(array(
-            'post_type' => 'cac-store-locations', 
-            'posts_per_page' => -1 
-        ));
+            if ($location_posts->have_posts()):
+                echo '<section class="store-locations">';
+                while ($location_posts->have_posts()):
+                    $location_posts->the_post();
 
-        if ( $location_posts->have_posts() ) :
-            echo '<div class="store-locations">';
-            while ( $location_posts->have_posts() ) : $location_posts->the_post();
+                    // Fetch fields for each location
+    
+                    $location_name = get_field('location_name');
+                    $location_hours = get_field('location_hours');
+                    $location_phone = get_field('location_phone');
+                    $location_address = get_field('location_address');
 
-                // Fetch fields for each location
-                
-                $location_name = get_field('location_name');
-                $location_hours = get_field('location_hours');
-                $location_phone = get_field('location_phone');
-                $location_address = get_field('location_address');
-
-                echo '<div class="store">';
-                // Display location image
-                $location_image_id = get_post_meta( get_the_ID(), 'location_image', true ); 
-                if ( $location_image_id ) {
-                    $location_image_url = wp_get_attachment_image_url( $location_image_id, 'full' );
-                    echo '<img src="' . esc_url($location_image_url) . '" alt="' . esc_attr(get_the_title()) . '" />';
-                }
-                // Display other location details
-                echo '<h2>' . esc_html($location_name) . '</h2>';
-                echo '<p>Hours: ' . esc_html($location_hours) . '</p>';
-                echo '<p>Phone: ' . esc_html($location_phone) . '</p>';
-                echo '<p>Address: ' . esc_html($location_address) . '</p>';
-                echo '</div>';
-
-            endwhile;
-            echo '</div>';
-        endif;
-        wp_reset_postdata();
-
-        // Display Store Locations Map
-        if ( get_field('store_locations_map') ): 
-            echo '<div class="store-locations-map">' . get_field('store_locations_map') . '</div>';
-        endif;
-
-        // Display Social Media Message
-        if ( get_field('social_media_message') ): 
-            echo '<div class="social-media-message">' . get_field('social_media_message') . '</div>';
-        endif;
-      
-        // Display Social Media Links
-        if ( have_rows('social_media_links') ): 
-            echo '<div class="social-media-links">';
-            while ( have_rows('social_media_links') ) : the_row();
-                $social_media_link_url = get_sub_field('social_media_link');
-                $social_media_image_id = get_sub_field('social_media_image');
-
-                echo '<div class="social-media-item">';
-                if ( $social_media_link_url ) {
-                    echo '<a href="' . esc_url($social_media_link_url) . '">';
-                    if ( $social_media_image_id ) {
-                        $social_media_image_url = wp_get_attachment_image_url( $social_media_image_id, 'full' );
-                        $social_media_image_alt = get_post_meta( $social_media_image_id, '_wp_attachment_image_alt', true );
-                        echo '<img src="' . esc_url($social_media_image_url) . '" alt="' . esc_attr($social_media_image_alt) . '" />';
+                    echo '<article class="store">';
+                    // Display location image
+                    $location_image_id = get_post_meta(get_the_ID(), 'location_image', true);
+                    if ($location_image_id) {
+                        $location_image_url = wp_get_attachment_image_url($location_image_id, 'full');
+                        echo '<img src="' . esc_url($location_image_url) . '" alt="' . esc_attr(get_the_title()) . '" />';
                     }
-                    echo '</a>';
-                }
-                echo '</div>';
-            endwhile;
-            
-            
-            echo '</div>';
-        endif;
-        
+                    // Display other location details
+                    echo '<h2>' . esc_html($location_name) . '</h2>';
+                    echo '<p>Hours: ' . esc_html($location_hours) . '</p>';
+                    echo '<p>Phone: ' . esc_html($location_phone) . '</p>';
+                    echo '<p>Address: ' . esc_html($location_address) . '</p>';
+                    echo '</article>';
+
+                endwhile;
+                echo '</section>';
+            endif;
+            wp_reset_postdata();
+
+            // Display Store Locations Map
+            if (get_field('store_locations_map')):
+                echo '<section class="store-locations-map">' . get_field('store_locations_map') . '</section>';
+            endif;
+
+            // Display Social Media Message
+            if (get_field('social_media_message')):
+                echo '<p class="social-media-message">' . get_field('social_media_message') . '</p>';
+            endif;
+
+            // Display Social Media Links
+            if (have_rows('social_media_links')):
+                echo '<section class="social-media-links">';
+                while (have_rows('social_media_links')):
+                    the_row();
+                    $social_media_link_url = get_sub_field('social_media_link');
+                    $social_media_image_id = get_sub_field('social_media_image');
+
+                    echo '<div class="social-media-item">';
+                    if ($social_media_link_url) {
+                        echo '<a href="' . esc_url($social_media_link_url) . '">';
+                        if ($social_media_image_id) {
+                            $social_media_image_url = wp_get_attachment_image_url($social_media_image_id, 'full');
+                            $social_media_image_alt = get_post_meta($social_media_image_id, '_wp_attachment_image_alt', true);
+                            echo '<img src="' . esc_url($social_media_image_url) . '" alt="' . esc_attr($social_media_image_alt) . '" />';
+                        }
+                        echo '</a>';
+                    }
+                    echo '</div>';
+                endwhile;
+
+
+                echo '</section>';
+            endif;
+        }
         // The default page content
-        get_template_part( 'template-parts/content', 'page' );
+        get_template_part('template-parts/content', 'page');
 
         // If comments are open or we have at least one comment, load up the comment template.
-        if ( comments_open() || get_comments_number() ) :
+        if (comments_open() || get_comments_number()):
             comments_template();
         endif;
 
     endwhile; // End of the loop.
-    gravity_form( 2, false, false, false, '', false );
+    gravity_form(2, false, false, false, '', false);
     ?>
 
 </main>
