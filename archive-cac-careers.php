bbaf39612f6fe8e4a9e4838473b43b246721ec04
cat <macy.cordner@gmail.com>
2023-11-16 22:18:22 -0800

    added if function_exists to careers

--- archive-cac-careers.php
+++ archive-cac-careers.php
@@ -46,42 +46,44 @@ get_header();
 					)
 				);
 				//array_unique removes all duplicates and array_map takes the post id and returns the location name 
-				$store_locations = array_unique(array_map(function ($post_id) {
-					return get_field('location_name', $post_id);
-				}, $store_locations));
+				if (function_exists("get_field")) {
+					$store_locations = array_unique(array_map(function ($post_id) {
+						return get_field('location_name', $post_id);
+					}, $store_locations));
 
-				// Output radio buttons
-				foreach ($store_locations as $location): ?>
+					// Output radio buttons
+					foreach ($store_locations as $location): ?>
+						<label>
+							<input type="radio" name="location" value="<?php echo esc_attr($location); ?>">
+							<?php echo esc_html($location); ?>
+						</label>
+					<?php endforeach; ?>
 					<label>
-						<input type="radio" name="location" value="<?php echo esc_attr($location); ?>">
-						<?php echo esc_html($location); ?>
+						<input type="radio" name="location" value="All" checked> All
 					</label>
-				<?php endforeach; ?>
-				<label>
-					<input type="radio" name="location" value="All" checked> All
-				</label>
 
-			</form>
+				</form>
 
-			<div id="filtered-jobs">
-				<?php
-				while ($careers_query->have_posts()):
-					$careers_query->the_post();
+				<div id="filtered-jobs">
+					<?php
+					while ($careers_query->have_posts()):
+						$careers_query->the_post();
 
-					$location_name = get_field('location_name');
-					$job_descriptions = get_field('job_descriptions');
-					$job_cta = get_field('job_cta');
+						$location_name = get_field('location_name');
+						$job_descriptions = get_field('job_descriptions');
+						$job_cta = get_field('job_cta');
 
-					// Add a class based on location to each job-information div
-					echo '<article class="job-information location-' . sanitize_title($location_name) . '">';
-					echo '<h2>' . esc_html(get_the_title()) . '</h2>';
-					echo esc_html($location_name);
-					echo $job_descriptions;
-					echo '<a href="' . esc_url($job_cta) . '">Apply Now</a>';
-					echo '</article>';
-				endwhile;
-
+						// Add a class based on location to each job-information div
+						echo '<article class="job-information location-' . sanitize_title($location_name) . '">';
+						echo '<h2>' . esc_html(get_the_title()) . '</h2>';
+						echo esc_html($location_name);
+						echo $job_descriptions;
+						echo '<a href="' . esc_url($job_cta) . '">Apply Now</a>';
+						echo '</article>';
+					endwhile;
+				}
 				wp_reset_postdata();
+
 				?>
 			</div>
 
