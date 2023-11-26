daf72b028e13beafc80f9884680b5f1428412452
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-25 19:27:01 -0800

    added basic style to footer mobile

--- footer.php
+++ footer.php
@@ -13,24 +13,19 @@
 
 <footer id="colophon" class="site-footer">
 	<!-- Output the secondary logo -->
-	<?php 
-    $secondary_logo_url = get_theme_mod( 'secondary-logo' );
-    if ($secondary_logo_url )
-       echo '<img src="'.$secondary_logo_url.'" alt = "secondary logo" class="secondary-logo">';
-    ?>
-	<div class="footer-menus">
-		<nav class="footer-navigation">
-			<?php wp_nav_menu(array('theme_location' => 'footer-left')); ?>
+	<?php
+		$secondary_logo_url = get_theme_mod('secondary-logo');
 
-		</nav>
+		if ($secondary_logo_url) {
+			// Get the homepage URL
+			$homepage_url = esc_url(home_url('/'));
 
-		<nav class='footer-navigation'>
-			<?php wp_nav_menu(array('theme_location' => 'footer-right')); ?>
-		</nav>
-	</div><!-- .footer-menus -->
-	<div class="site-info">
-		<ul class="team-list">
-
+			// Output the anchor tag with the secondary logo
+			echo '<a class="secondary-logo-link" href="' . $homepage_url . '"><img src="' . $secondary_logo_url . '" alt="secondary logo" class="secondary-logo"></a>';
+		}
+	?>
+	<div class="footer-left-section">
+		<!-- <ul class="team-list">
 
 			<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_1')); ?>">
 					<?php esc_html_e('Team Member 1', 'crust-crumb'); ?>
@@ -47,12 +42,24 @@
 			<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_5')); ?>">
 					<?php esc_html_e('Team Member 5', 'crust-crumb'); ?>
 				</a></li>
-		</ul>
-		<!-- need to make privacy policy page -->
-		<p>
-			<?php the_privacy_policy_link() ?>
-		<p>
-	</div><!-- .site-info -->
+		</ul> -->
+	
+		<h2>About Crust and Crumb</h2>
+		<p class='about-us-brief'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+		<?php $year = (new DateTime)->format("Y"); ?>
+		<p class='copy-right'>&copy; <?php echo $year; ?> Made by U.B.w.C</p>
+	</div>
+	<div class="footer-right-section">
+		<nav class="footer-navigation">
+			<?php wp_nav_menu(array('theme_location' => 'footer-left')); ?>
+
+		</nav>
+
+		<nav class='footer-navigation'>
+			<?php wp_nav_menu(array('theme_location' => 'footer-right')); ?>
+		</nav>
+	</div><!-- .footer-menus -->
+	
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
