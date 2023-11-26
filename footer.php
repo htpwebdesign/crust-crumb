1aacc8da8ea8fb1a0c5bd7f3f0bfb46ef9a9b357
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-25 20:00:23 -0800

    added more style for footer

--- footer.php
+++ footer.php
@@ -24,42 +24,43 @@
 			echo '<a class="secondary-logo-link" href="' . $homepage_url . '"><img src="' . $secondary_logo_url . '" alt="secondary logo" class="secondary-logo"></a>';
 		}
 	?>
-	<div class="footer-left-section">
-		<!-- <ul class="team-list">
+	<div class='footer-flex-box'>
+		<div class="footer-left-section">
+			<!-- <ul class="team-list">
 
-			<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_1')); ?>">
-					<?php esc_html_e('Team Member 1', 'crust-crumb'); ?>
-				</a></li>
-			<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_2')); ?>">
-					<?php esc_html_e('Team Member 2', 'crust-crumb'); ?>
-				</a></li>
-			<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_3')); ?>">
-					<?php esc_html_e('Team Member 3', 'crust-crumb'); ?>
-				</a></li>
-			<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_4')); ?>">
-					<?php esc_html_e('Team Member 4', 'crust-crumb'); ?>
-				</a></li>
-			<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_5')); ?>">
-					<?php esc_html_e('Team Member 5', 'crust-crumb'); ?>
-				</a></li>
-		</ul> -->
-	
-		<h2>About Crust and Crumb</h2>
-		<p class='about-us-brief'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
-		<?php $year = (new DateTime)->format("Y"); ?>
-		<p class='copy-right'>&copy; <?php echo $year; ?> Made by U.B.w.C</p>
+				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_1')); ?>">
+						<?php esc_html_e('Team Member 1', 'crust-crumb'); ?>
+					</a></li>
+				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_2')); ?>">
+						<?php esc_html_e('Team Member 2', 'crust-crumb'); ?>
+					</a></li>
+				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_3')); ?>">
+						<?php esc_html_e('Team Member 3', 'crust-crumb'); ?>
+					</a></li>
+				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_4')); ?>">
+						<?php esc_html_e('Team Member 4', 'crust-crumb'); ?>
+					</a></li>
+				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_5')); ?>">
+						<?php esc_html_e('Team Member 5', 'crust-crumb'); ?>
+					</a></li>
+			</ul> -->
+		
+			<h2>About Crust and Crumb</h2>
+			<p class='about-us-brief'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+			<?php $year = (new DateTime)->format("Y"); ?>
+			<p class='copy-right'>&copy; <?php echo $year; ?> Made by U.B.w.C</p>
+		</div>
+		<div class="footer-right-section">
+			<nav class="footer-navigation">
+				<?php wp_nav_menu(array('theme_location' => 'footer-left')); ?>
+
+			</nav>
+
+			<nav class='footer-navigation'>
+				<?php wp_nav_menu(array('theme_location' => 'footer-right')); ?>
+			</nav>
+		</div>
 	</div>
-	<div class="footer-right-section">
-		<nav class="footer-navigation">
-			<?php wp_nav_menu(array('theme_location' => 'footer-left')); ?>
-
-		</nav>
-
-		<nav class='footer-navigation'>
-			<?php wp_nav_menu(array('theme_location' => 'footer-right')); ?>
-		</nav>
-	</div><!-- .footer-menus -->
-	
 </footer><!-- #colophon -->
 </div><!-- #page -->
 
