f897833603d065d546bd048b20a3efbcd9fab923
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-27 15:01:02 -0800

    Adjusted the content in 404 page

--- 404.php
+++ 404.php
@@ -23,33 +23,10 @@ get_header();
 					<?php
 					get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'crust-crumb' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
+					<h2>Go to <a href="<?php echo esc_url(home_url('/')); ?>">Home</a> or <a href="<?php echo esc_url(home_url('/shop')); ?>">Menu</a> page.</h2>
 
-					<?php
-					/* translators: %1$s: smiley */
-					$crust_crumb_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'crust-crumb' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$crust_crumb_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
