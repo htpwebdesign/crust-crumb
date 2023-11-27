384ceb22ddc56ee94674c9de95fb5049c230876a
Joaquin Opulencia <joaquinopulencia99@gmail.com>
2023-11-27 11:41:21 -0800

    hamburger styles and link colors

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,25 +1,29 @@
-const hamburgerMenu = () => {
-    const siteNavigation = document.getElementById( 'site-navigation' );
-    const hamburgerBtn = document.querySelector('.hamburger');
+window.onload = function () {
+	const siteNavigation = document.getElementById( 'site-navigation' );
+	const menu_btn = document.querySelector('.hamburger');
 
-   
-    // add toggle and accessibility
-	button.addEventListener( 'click', function() {
-		siteNavigation.classList.toggle( 'toggled' );
-        hamburgerBtn.classList.toggle('is-active');
-		if ( button.getAttribute( 'aria-expanded' ) === 'true' ) {
-			button.setAttribute( 'aria-expanded', 'false' );
-		} else {
-			button.setAttribute( 'aria-expanded', 'true' );
-		}
-	} );
 
-    // remove
-	document.addEventListener( 'click', function( event ) {
-		const isClickInside = siteNavigation.contains( event.target );
-		if ( ! isClickInside ) {
-			siteNavigation.classList.remove( 'toggled' );
-			button.setAttribute( 'aria-expanded', 'false' );
-		}
-	} );
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+	});
+
+// add toggle and accessibility
+button.addEventListener( 'click', function() {
+    siteNavigation.classList.toggle( 'toggled' );
+    if ( button.getAttribute( 'aria-expanded' ) === 'true' ) {
+        button.setAttribute( 'aria-expanded', 'false' );
+    } else {
+        button.setAttribute( 'aria-expanded', 'true' );
+    }
+} );
+
+// remove
+document.addEventListener( 'click', function( event ) {
+    const isClickInside = siteNavigation.contains( event.target );
+    if ( ! isClickInside ) {
+        siteNavigation.classList.remove( 'toggled' );
+        button.setAttribute( 'aria-expanded', 'false' );
+    }
+} );
 }
