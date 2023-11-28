a7949a0295c5c7bc5c218b1e292010ba29741075
cat <macy.cordner@gmail.com>
2023-11-28 14:05:27 -0800

    changed job filtering to no isoptope

--- js/filter-jobs.js
+++ js/filter-jobs.js
@@ -7,19 +7,20 @@ document.addEventListener('DOMContentLoaded', function () {
             // Get the value of the selected radio button
             const selectedLocation = document.querySelector('input[name="location"]:checked').value;
 
-            // // Remove 'is-checked' class from all radio buttons
-            // radioButtons.forEach(function (r) {
-            //     r.classList.remove('is-checked');
-            // });
+            // Remove 'is-checked' class from all radio buttons
+            radioButtons.forEach(function (r) {
+                r.classList.remove('is-checked');
+            });
 
-            // // Add 'is-checked' class to the selected radio button
-            // radio.classList.add('is-checked');
+            // Add 'is-checked' class to the selected radio button
+            radio.classList.add('is-checked');
             // Select all elements with the class "job-information"
             const jobDescriptions = document.querySelectorAll('.job-information');
             // Iterate over each job description element
             jobDescriptions.forEach(function (job) {
                 // get location ACF and trim any whitespace if needed
-                const location = job.querySelector('.location').textContent.trim();
+                const location = job.querySelector('.career-location').textContent.trim();
+
                 // Check if the selected location is "All" or if the job location includes the selected location
                 if (selectedLocation === 'All' || location.toLowerCase().includes(selectedLocation.toLowerCase())) {
                     // display jobs with selectedLocation
