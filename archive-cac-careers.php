e96a54f166d732c7ea4f084c7e70be67ef376f9a
cat <macy.cordner@gmail.com>
2023-11-29 13:46:14 -0800

    fixed the order notes from moving around

--- archive-cac-careers.php
+++ archive-cac-careers.php
@@ -77,8 +77,8 @@ get_header();
 					$job_cta = get_field('job_cta');
 
 					// Add a class based on location to each job-information div
-					echo '<article class="job-information accordion-container location-' . sanitize_title($location_name) . ' fade-up ">';
-					echo '<h2 class="accordionTitle">' . esc_html(get_the_title());
+					echo '<article tabindex="0" class="job-information accordion-container location-' . sanitize_title($location_name) . ' fade-up ">';
+					echo '<h2  class="accordionTitle">' . esc_html(get_the_title());
 					echo '<span class="accordion-icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 9h-9v-9h-6v9h-9v6h9v9h6v-9h9z"/></svg></span></h2>';
 					echo '<div class="accordionContent">';
 					echo '<b class="career-location">' . esc_html($location_name) . '</b>';
