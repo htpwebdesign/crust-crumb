d3634f54d52b6fbfe5f87784b1f09cb12122d297
cat <macy.cordner@gmail.com>
2023-11-29 12:55:09 -0800

    fixed margin on locations for desktop, changed wording of 'shipping' to 'delivery', fixed careers link in about page

--- functions.php
+++ functions.php
@@ -203,6 +203,9 @@ function crust_crumb_scripts()
 
 
 }
+
+
+
 add_action('wp_enqueue_scripts', 'crust_crumb_scripts');
 
 /**
@@ -243,3 +246,5 @@ if (defined('JETPACK__VERSION')) {
 if (class_exists('WooCommerce')) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+
