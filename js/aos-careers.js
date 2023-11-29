b7fce38b380b646f5419df5cfa64e816aa8837d4
cat <macy.cordner@gmail.com>
2023-11-28 18:47:37 -0800

    fixed aos on careers

--- js/aos-careers.js
+++ js/aos-careers.js
@@ -4,7 +4,10 @@ jQuery(document).ready(function ($) {
         // Remove AOS classes from all elements
         $('[data-aos]').removeClass('aos-init aos-animate');
 
-        // Apply AOS classes to the visible elements
-        $('[data-aos]:visible').addClass('aos-init aos-animate');
+        // Delay the reapplication of AOS classes
+        setTimeout(function () {
+            // Apply AOS classes to the visible elements
+            $('[data-aos]:visible').addClass('aos-init aos-animate');
+        }, 100); // You can adjust the delay as needed
     });
 });
