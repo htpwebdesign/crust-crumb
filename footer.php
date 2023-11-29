22b621334ee0dd4232f62fe3a46747fe71bc15e9
jackdo812 <137816079+jackdo812@users.noreply.github.com>
2023-11-29 10:39:25 -0800

    added about-us-footer content, adjusted cart item count and menu instructions

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -14,39 +15,27 @@
 <footer id="colophon" class="site-footer">
 	<!-- Output the secondary logo -->
 	<?php
-		$secondary_logo_url = get_theme_mod('secondary-logo');
+	$secondary_logo_url = get_theme_mod('secondary-logo');
 
-		if ($secondary_logo_url) {
-			// Get the homepage URL
-			$homepage_url = esc_url(home_url('/'));
+	if ($secondary_logo_url) {
+		// Get the homepage URL
+		$homepage_url = esc_url(home_url('/'));
 
-			// Output the anchor tag with the secondary logo
-			echo '<a class="secondary-logo-link" href="' . $homepage_url . '"><img src="' . $secondary_logo_url . '" alt="secondary logo" class="secondary-logo"></a>';
-		}
+		// Output the anchor tag with the secondary logo
+		echo '<a class="secondary-logo-link" href="' . $homepage_url . '"><img src="' . $secondary_logo_url . '" alt="secondary logo" class="secondary-logo"></a>';
+	}
 	?>
 	<div class='footer-flex-box'>
 		<div class="footer-left-section">
-			<!-- <ul class="team-list">
 
-				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_1')); ?>">
-						<?php esc_html_e('Team Member 1', 'crust-crumb'); ?>
-					</a></li>
-				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_2')); ?>">
-						<?php esc_html_e('Team Member 2', 'crust-crumb'); ?>
-					</a></li>
-				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_3')); ?>">
-						<?php esc_html_e('Team Member 3', 'crust-crumb'); ?>
-					</a></li>
-				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_4')); ?>">
-						<?php esc_html_e('Team Member 4', 'crust-crumb'); ?>
-					</a></li>
-				<li><a href="<?php echo esc_url(__('link_to_linkedin_profile_5')); ?>">
-						<?php esc_html_e('Team Member 5', 'crust-crumb'); ?>
-					</a></li>
-			</ul> -->
-		
 			<h2>About Crust and Crumb</h2>
-			<p class='about-us-brief'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </p>
+			<?php
+			if (function_exists('get_field')) {
+				if (get_field('about_us_footer', 25)) {
+					echo '<p class="about-us-brief">' . get_field('about_us_footer', 25) . '</p>';
+				}
+			}
+			?>
 			<?php $year = (new DateTime)->format("Y"); ?>
 			<p class='copy-right'>&copy; <?php echo $year; ?> Made by U.B.w.C</p>
 		</div>
